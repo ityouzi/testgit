f7723aff127f45d17411203100a64d1d831109b9
youzi <2504480136@qq.com>
2018-11-20 16:24:35 +0800

    更新1

--- family/js/public.js
+++
@@ -1,6 +0,0 @@
-//var baseUrl = "http://192.168.0.250:8081";
-//var baseUrl = "http://119.97.180.51:914";
-/*var baseUrl = "http://192.168.0.106:8088";*/
-//var baseUrl = "http://127.0.0.1:8080";
-//var baseUrl = "http://192.168.0.102:8080";
-var baseUrl = "http://192.168.0.208:8088";
